1c384030a538c51e2e7f4e4617c9816a74acc28c
Yann Nitzpon <ynitzpon@gmail.com>
2021-11-03 21:12:19 +0100

    Added Spendees to expense report

--- expense.py
+++ expense.py
@@ -18,6 +18,11 @@ expense_questions = [
         "name":"spender",
         "message":"New Expense - Spender: ",
     },
+    {
+        "type":"input",
+        "name":"spendees",
+        "message":"New Expense - Spendees: ",
+    },
 
 ]
 
@@ -28,26 +33,44 @@ def new_expense(*args):
     amount = infos.get('amount')
     label = infos.get('label')
     spender = infos.get('spender')
+    spendee_list = str(infos.get('spendees'))
+    # print("Spendees : ", spendee_list, file=sys.stderr)
+    spendees = list(csv.reader([spendee_list]))[0]
+    # print("Spendees : ", spendees, file=sys.stderr)
     
-    # Check if user exists
-    ## Put all the users in a set 
+    # Checks
+    ## Check if spender exists (if it's an existing user)
+    ### Put all the users in a set 
     users = { '' }
     users.clear()
 
-    file = open("users.csv", "r")
+    with open("users.csv", "r") as file:
+        for line in file:
+            line = line[:-1]
+            users.add(line)
 
-    for line in file:
-        line = line[:-1]
-        users.add(line)
-
-    print("Users : ", users, file=sys.stderr)
+    # print("Users : ", users, file=sys.stderr)
     
     if (spender not in users):
         print("You can't add an expense from an unknown user !")
         return False
     
+    ## Check if Spender is amongst Spendee
+    
+    if (spender not in spendees):
+        print("Spender : ", spender, file=sys.stderr)
+        print("Spendees : ", spendees, file=sys.stderr)
+        print("The spender has to be amongst the spendees")
+        return False
+    
+    ## Check if all spendees are users
+    for spender in spendees:
+        if (spender not in users):
+            print("All the spendees have to be users")
+            return False
+    
     # Creates the line to add to the csv file
-    data = [ amount, label, spender]
+    data = [ amount, label, spender, spendee_list]
     
     # Writes the informations on external file : expense_report.csv
     with open('expense_report.csv', 'a', encoding='UTF8', newline='') as file:
